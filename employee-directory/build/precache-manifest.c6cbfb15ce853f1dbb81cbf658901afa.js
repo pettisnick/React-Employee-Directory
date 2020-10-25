ef4488a1c0e24e9199dd3875f3ce3bf6b90348aa
Nick Pettis <pettisnick@gmail.com>
2020-10-24 21:20:11 -0400

    files structure

---
+++ employee-directory/build/precache-manifest.c6cbfb15ce853f1dbb81cbf658901afa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96539980ecb21435a98b995c8998ca66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b734a3766d803e8b01b",
+    "url": "/static/css/2.99a2b36c.chunk.css"
+  },
+  {
+    "revision": "ae855fb26cf6e9665d0d",
+    "url": "/static/css/main.63308485.chunk.css"
+  },
+  {
+    "revision": "9b734a3766d803e8b01b",
+    "url": "/static/js/2.874249f3.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/static/js/2.874249f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae855fb26cf6e9665d0d",
+    "url": "/static/js/main.3e1bae0a.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
