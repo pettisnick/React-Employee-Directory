49baa9d5f0028c2cbc72a107d706dc7d4db08d8a
Nick Pettis <pettisnick@gmail.com>
2020-10-24 23:29:55 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Wrapper from "./components/Wrapper";
 import Home from "./pages/Home";
