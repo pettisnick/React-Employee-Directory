6c655caf91f7ec14dad9a8ecf217f7267e99ff00
Nick Pettis <pettisnick@gmail.com>
2020-10-26 15:18:47 -0400

    Updates

---
+++ precache-manifest.ac19c91699cf0612595ca05dce6594ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ad5c1696445c838a5130f13cc25acd",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "4a14918798a29b6d742e",
+    "url": "/React-Employee-Directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "4856893ce2aa9f44a770",
+    "url": "/React-Employee-Directory/static/css/main.50d77cc9.chunk.css"
+  },
+  {
+    "revision": "4a14918798a29b6d742e",
+    "url": "/React-Employee-Directory/static/js/2.cb8474d8.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/React-Employee-Directory/static/js/2.cb8474d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4856893ce2aa9f44a770",
+    "url": "/React-Employee-Directory/static/js/main.f79da9fc.chunk.js"
+  },
+  {
+    "revision": "d5a550fc769b1b6308d1",
+    "url": "/React-Employee-Directory/static/js/runtime-main.f187368d.js"
+  }
+]);
