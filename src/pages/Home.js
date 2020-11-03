a4663febfdf5a342cb1548044e3f02da8d0ae7c6
Nick Pettis <pettisnick@gmail.com>
2020-11-02 21:35:19 -0500

    filtering employees

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,20 +6,39 @@ import Table from "../components/Table";
 
 
 function Home() {
+  //set up State for any variable that we expect to change together
+  //useState is expected to return an array 
   const [employees, setEmployees] = useState([]);
+  //useState is expected to return a string
   const [search, setSearch] = useState("");
   const [results, setResults] = useState([]);
+  
 
+  //similar to componentDidMount, makes an API call and render some data as soon as the component loads
+  //useEffect takes in a parameter that is a function, and the function runs as soon as the component mounts
   useEffect(() => {
-    //using the employee info from the API 
-    API.getEmpoloyeeInfo()
+    //'search' represents the current state of our searchState, searchTerm
+    if (!search) {
+      return;
+    }
+    //getEmployeeInfo is the API function name 
+    API.getEmpoloyeeInfo(search)
       .then((res) => {
-        //returns the employees and all the results
+        if (res.data.length === 0) {
+          //throw error in body of function
+          throw new Error("No results found!");
+        }
+        //if we get an error
+        if (res.data.status === "error") {
+          throw new Error(res.data.message);
+        }
+        //set state after we get response and makes app re-render
         setEmployees(res.data.results);
         setResults(res.data.results);
       })
       .catch(err => console.log(err));
-  }, []);
+      //everytime you type in a new character, you are changing the value of the variable which is prompting the useEffect to run 
+  }, [search]);
 
   const filterEmployees = () => {
     return employees.filter(
